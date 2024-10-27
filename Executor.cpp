ab91bbef5a3426641ae7581bb53a146ee708b60f
hcharise <110205350+hcharise@users.noreply.github.com>
2024-10-27 17:04:39 -0600

    Update Executor.cpp

--- Executor.cpp
+++ Executor.cpp
@@ -8,16 +8,17 @@ Executor::Executor(void* testFunction, vector<void*> functionParams, void* funct
 
 void Executor::execute() {
     try {
-        // call function here
+        testFunction();
     } catch(const std::exception& e) {
         std::cerr << e.what() << '\n';
     }
-    
+
+    // need to save results of testFunction and call assess?
 }
 
 void Executor::assess() {
     // compare results to asserts
-    // print results to ResultLog
+    // call ResultLog?
 }
 
 ResultLog Executor::packageResults() {
