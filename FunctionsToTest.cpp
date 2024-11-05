e42c1df35d57923017265961073660ab82f60866
hcharise <110205350+hcharise@users.noreply.github.com>
2024-11-05 11:49:25 -0700

    Deleted previous creation of vector/passing to test harness.

--- FunctionsToTest.cpp
+++ FunctionsToTest.cpp
@@ -67,27 +67,6 @@ int main() {
     };
     tests.push_back(lam_Eq_Num_Fail);
 
-    /*
-    vector<vector<void*>> params;
-    int num1 = 10;
-    int num2 = 5;
-    string name = "Steve";
-
-    vector<void*> temp;
-    temp.push_back(&num1);
-    temp.push_back(&num2);
-    temp.push_back(&name);
-    params.push_back(temp);
-
-    vector<void*> asserts;
-    int output = 10;
-    asserts.push_back(&output);
-
-    hello(num1, num2, name);
-    TestHarness testHarness = TestHarness((void*)hello, params, asserts, 1);
-    testHarness.runAllTests();
-    testHarness.printOutResults();
-
-    */
+    
     return 0;
 }
