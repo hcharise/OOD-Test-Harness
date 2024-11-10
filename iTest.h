10d4d71b98c29cf683bb05d8aa9940496382e77b
Katrina Luu <48665387+midgetalchemist@users.noreply.github.com>
2024-11-10 15:41:52 -0800

    Added incorrect functions

--- iTest.h
+++ iTest.h
@@ -10,22 +10,34 @@ public:
 	virtual ~iTest() {}
 };
 
-class Positive_Number : public iTest {
+class Pos_Num : public iTest {
 private:
 	int num;
 
 public:
-	Positive_Number(int n) : num(n) {}
+	Pos_Num(int n) : num(n) {}
 	bool test() override {
 		return num > 0;
 	}
 };
 
-class Empty_String : public iTest {
+class Neg_Num : public iTest {
+private:
+	int num;
+
+public:
+	Neg_Num(int n) : num(n) {}
+
+	bool test() override { 
+		return num < 0;
+	}
+};
+
+class Empty_Str : public iTest {
 private:
 	std::string str;
 public:
-	Empty_String(const std::string& s) : str(s) {}
+	Empty_Str(const std::string& s) : str(s) {}
 	bool test() override {
 		return !str.empty();
 	}
@@ -37,6 +49,53 @@ private:
 public:
 	Equal_Num(int x, int y) : a(x), b(y) {}
 	bool test() override {
+		return a == b;
+	}
+};
+
+//Error: Unnecessary parameter in test()
+class E_Pos_Num : public iTest {
+private:
+	int num;
+
+public:
+	E_Pos_Num(int n) : num(n) {}
+	bool test(int n) override {
+		return num > 0;
+	}
+};
+
+//Error: Adding cost
+class E_Neg_Num : public iTest {
+private:
+	int num;
+
+public:
+	E_Neg_Num(int n) : num(n) {}
+
+	bool test() const override {
+		return num < 0;
+	}
+};
+
+//Error: Typo
+class E_Empty_Str : public iTest {
+private:
+	std::string str;
+public:
+	E_Empty_Str(const std::string& s) : str(s) {}
+	bool Test() override {
+		return !str.empty();
+	}
+};
+
+//Error: Wrong return type
+class E_Equal_Num : public iTest {
+private:
+	int a, b;
+public:
+	E_Equal_Num(int x, int y) : a(x), b(y) {}
+	int test() override {
 		return a == b;
 	}
 };
