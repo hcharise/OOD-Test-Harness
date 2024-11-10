e8affcd9618f5a035683af1db4fc228dd41debc9
AamyAR <aamanisuvarna2602@gmail.com>
2024-11-10 17:54:41 +0530

    Update ResultLog.h
    removed the I/O logs

--- ResultLog.h
+++ ResultLog.h
@@ -16,17 +16,11 @@ class ResultLog {
 		ResultLog(bool passed, const std::string& message);
 		bool didPass() const; // Fetch test log data
 		std::string getMessage() const;
-		std::vector < std::string > getInputs() const;
-		std::string getExpectedOutput() const;
-		std::string getActualOutput() const; //need to discuss on this
 		std::string getTimestamp() const;
 		std::string getLogDetails(LogLevel logLevel) const; // Method to display results based on log level
 	private:
 		bool passed; // Indicates if the test passed
 		std::string message; // Message with specific information or errors
-		std::vector < std::string > inputs; // Input parameters for the test
-		std::string expectedOutput; // Expected output for the test
-		std::string actualOutput; // Actual output from the test
 		std::string timestamp; // Timestamp of the test execution
 		std::string generateTimestamp() const; // Generate the current timestamp
 };
