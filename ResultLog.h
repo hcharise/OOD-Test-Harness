c0d7d307d153262455ce6783cb05cea63cf7e7bb
AamyAR <aamanisuvarna2602@gmail.com>
2024-12-02 14:36:27 +0530

    Update ResultLog.h
    Serialize  and Deserialize of  JSON string added

--- ResultLog.h
+++ ResultLog.h
@@ -4,6 +4,9 @@
 #include <string>
 #include <vector>
 #include <ctime>  //For Date Time stamp
+#include <sstream>
+#include <iomanip>
+#include <json/json.h>  // Assuming JSON serialization (Boost or other JSON libraries*)
 
 enum class LogLevel {
 	PASS_FAIL, // Only logs pass/fail status
@@ -13,11 +16,16 @@ enum class LogLevel {
 
 class ResultLog {
 	public:
-		ResultLog(bool passed, const std::string& message);
+		ResultLog();
+    		ResultLog(bool passed, const std::string& message);
 		bool didPass() const; // Fetch test log data
 		std::string getMessage() const;
 		std::string getTimestamp() const;
 		std::string getLogDetails(LogLevel logLevel) const; // Method to display results based on log level
+
+		// Serialization/Deserialization
+    		std::string serialize() const;          // Converts the log to a JSON string
+    		static ResultLog deserialize(const std::string& data); // Parses a JSON string to create a ResultLog object
 	private:
 		bool passed; // Indicates if the test passed
 		std::string message; // Message with specific information or errors
